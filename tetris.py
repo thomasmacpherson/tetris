a8950ff58e3650746d60536f29bfd3b144dea165
thomasmacpherson <thomasmacpherson@live.co.uk>
2012-08-02 13:38:16 +0100

    adding shadow blocks

--- tetris.py
+++ tetris.py
@@ -2,7 +2,7 @@
 
 from shapes import *
 bg_image = "background.png"
-
+from math import log10
 
 scr_pos_x = -200
 scr_pos_y = 0
@@ -29,6 +29,7 @@ class Block(object):
 		screen.blit(block_colours[self.colour],(self.x,self.y))
 class Shape(object):
 	global shape_index
+	global shadow_block
 	def __init__(self, x, y, shape):
 		self.x = x
 		self.y = y
@@ -38,12 +39,18 @@ class Shape(object):
 		self.rot = rot
 		self.shape = shape
 
-		
-		self.blocks = [
-			Block(x+shape_index[shape][rot][0][0],y+shape_index[shape][rot][1][0],shape),
-			Block(x+shape_index[shape][rot][0][1],y+shape_index[shape][rot][1][1],shape),
-			Block(x+shape_index[shape][rot][0][2],y+shape_index[shape][rot][1][2],shape),
-			Block(x+shape_index[shape][rot][0][3],y+shape_index[shape][rot][1][3],shape)]
+		if shape == 7:
+			self.blocks = [
+				Block(x+shape_index[active_block.shape][rot][0][0],y+shape_index[active_block.shape][rot][1][0],shape),
+				Block(x+shape_index[active_block.shape][rot][0][1],y+shape_index[active_block.shape][rot][1][1],shape),
+				Block(x+shape_index[active_block.shape][rot][0][2],y+shape_index[active_block.shape][rot][1][2],shape),
+				Block(x+shape_index[active_block.shape][rot][0][3],y+shape_index[active_block.shape][rot][1][3],shape)]
+		else:
+			self.blocks = [
+				Block(x+shape_index[shape][rot][0][0],y+shape_index[shape][rot][1][0],shape),
+				Block(x+shape_index[shape][rot][0][1],y+shape_index[shape][rot][1][1],shape),
+				Block(x+shape_index[shape][rot][0][2],y+shape_index[shape][rot][1][2],shape),
+				Block(x+shape_index[shape][rot][0][3],y+shape_index[shape][rot][1][3],shape)]
 
 
 
@@ -58,14 +65,17 @@ class Shape(object):
 			block.x += x
 			block.y += y
 
+
 		if check_collision():
 			for block in self.blocks:
 				block.x -= x
 				block.y -= y
+
 			if y != 0:
 				return True # new shape needed
 			#	new_shape()
-
+		else:
+			return False
 
 	def draw(self):
 		for block in self.blocks:
@@ -75,8 +85,8 @@ class Shape(object):
 		global y_count
 		for block in self.blocks:
 			y_count[(block.y-626)/32] +=1
-			print (block.y-626)/32
-			print y_count
+			#print (block.y-626)/32
+			#print y_count
 			dead_block_list.append(block)
 		check_lines()
 		del(self)
@@ -104,7 +114,7 @@ class Shape(object):
 		self.blocks[3].y = self.y + shape_index[self.shape][self.rot][1][3]
 
 		if check_collision():
-			print "Rotation collision"
+			#print "Rotation collision"
 			self.rot = previous_rot
 			for i, block in enumerate(self.blocks):
 				self.blocks[i].x = temp_blocks[i][0]
@@ -118,12 +128,14 @@ class Shape(object):
 def new_shape():
 	global active_block
 	global next_shape
+	global shadow_block
 	global next_shape_number
 	for block in active_block.blocks:
 		pass
 
 	active_block.delete()
 	active_block = Shape(scr_pos_x + 606,scr_pos_y + 18,next_shape_number)
+	shadow_block = Shape(scr_pos_x + 606, scr_pos_y + 18, 7)
 	next_shape_number = random.randint(0,6)
 	
 	while next_shape_number == active_block.shape:	# don't get the same block twice in a row
@@ -131,8 +143,18 @@ def new_shape():
 
 	if next_shape_number == 5:	# next shape box adjustment for line piece
 		next_shape = Shape(scr_pos_x + 926, scr_pos_y + 192, next_shape_number)
+	elif next_shape_number == 1:	# next shape box adjustment for square blocks
+		next_shape = Shape(scr_pos_x + 960, scr_pos_y + 200, next_shape_number)
 	else:
-		next_shape = Shape(scr_pos_x + 950, scr_pos_y + 215, next_shape_number)
+		next_shape = Shape(scr_pos_x + 943, scr_pos_y + 213, next_shape_number)
+
+
+
+	for x in range(1,20):
+		if shadow_block.y < (scr_pos_y + 594):
+			if shadow_block.move(0,32):
+				break
+
 
 
 def check_collision():
@@ -155,18 +177,19 @@ def check_lines():
 			check_list.append(block.y)
 	check_list.sort()
 	check_list.reverse()
-	print check_list
+	#print check_list
 	removed_line_count = 0
-
+	removed_list = list()
 	for y in check_list:
 		if check_line((y+(32*removed_line_count) - 82)/32):
+			removed_list
 			removed_line_count += 1
-
-	print "Removed line count: %d" %removed_line_count
+			# TODO: add flashing removal lines
+	#print "Removed line count: %d" %removed_line_count
 
 
 def check_line(line_number):
-	print line_number
+	#print line_number
 	global dead_block_list
 
 	global score
@@ -174,7 +197,7 @@ def check_line(line_number):
 	global lines
 	global y_count
 
-	print "Line number: %d" %line_number
+	#print "Line number: %d" %line_number
 	if y_count[line_number] > 9:
 		#y_count[line_number] = 0
 		lines += 1
@@ -213,7 +236,8 @@ def check_line(line_number):
 
 
 
-
+def draw_flashing_lines(line_list):
+	pass
 
 
 def draw_black_box():
@@ -237,6 +261,11 @@ red_block = pygame.image.load("pngs/tetblock4.png").convert()
 green_block = pygame.image.load("pngs/tetblock5.png").convert()
 purple_block = pygame.image.load("pngs/tetblock6.png").convert()
 turq_block = pygame.image.load("pngs/tetblock7.png").convert()
+clear_block = pygame.image.load("pngs/tetblockclear.png").convert()
+
+image1 = pygame.image.load("pngs/sacktetris.png").convert()
+image2 = pygame.image.load("pngs/placetetris.png").convert()
+image3 = pygame.image.load("pngs/oaktetris.png").convert()
 
 block_colours = [brown_block,
 		beige_block,
@@ -244,17 +273,30 @@ block_colours = [brown_block,
 		red_block,
 		green_block,
 		turq_block,
-		purple_block ]
+		purple_block,
+		clear_block ]
 
+background_images = [ 	image1,
+			image2,
+			image3 ]
 
 score = 0
 lines = 0
 level = 1
 
 clock = 0
-speed = 15
+speed = 15.0
 
 active_block = Shape(scr_pos_x + 606,scr_pos_y + 82, random.randint(0,6))
+
+shadow_block = Shape(scr_pos_x + 606,scr_pos_y + 82, random.randint(0,6))
+for x in range(1,20):
+	if shadow_block.y < (scr_pos_y + 594):
+		if shadow_block.move(0,32):
+			break
+
+
+
 next_shape_number = random.randint(0,6)
 
 while next_shape_number == active_block.shape:
@@ -262,8 +304,11 @@ while next_shape_number == active_block.shape:
 
 if next_shape_number == 5:
 	next_shape = Shape(scr_pos_x + 926, scr_pos_y + 192, next_shape_number)
+
+elif next_shape_number == 1:
+	next_shape = Shape(scr_pos_x + 965, scr_pos_y + 192, next_shape_number)
 else:
-	next_shape = Shape(scr_pos_x + 950, scr_pos_y + 215, next_shape_number)
+	next_shape = Shape(scr_pos_x + 948, scr_pos_y + 216, next_shape_number)
 time.sleep(2)
 
 running = True
@@ -279,6 +324,7 @@ while running == True:
 
 			elif event.key == K_RIGHT:# and active_block.x <766:
 				active_block.move(32,0)
+				shadow_block.move(32,0)
 
 			elif event.key == K_LEFT:# and active_block.x >510:
 				active_block.move(-32,0)
@@ -292,7 +338,7 @@ while running == True:
 						new_shape()
 
 			elif event.key == K_SPACE:
-				for x in range(1,17):
+				for x in range(1,18):
 					if active_block.y < (scr_pos_y + 594):
 						if active_block.move(0,32):
 							break
@@ -301,7 +347,11 @@ while running == True:
 
 	screen.blit(background, (scr_pos_x + 350,scr_pos_y + 50))
 
-	if clock % (speed/level) == 2:
+	screen.blit(background_images[level % len(background_images) ], (scr_pos_x + 510, scr_pos_y +49))
+	current_speed = speed/level
+	if current_speed < 2:
+		current_speed = 2
+	if clock % (current_speed) == 1:
 		if active_block.y < 594:
 			if active_block.move(0,32):
 				new_shape()
@@ -310,6 +360,7 @@ while running == True:
 
 	active_block.draw()
 	next_shape.draw()
+	shadow_block.draw()
 	
 	for block in dead_block_list:
 		block.draw()
@@ -317,12 +368,14 @@ while running == True:
 	display_score = font.render(str(score), 1, (0,0,0))
 	display_lines = font.render(str(lines), 1, (0,0,0))
 	display_level = font.render(str(level), 1, (0,0,0))
-
+	#+(10*int(log10(score)))
+	
 	screen.blit(display_score, (1030 + scr_pos_x, 358 + scr_pos_y))
 	screen.blit(display_level, (1030 + scr_pos_x, 456 + scr_pos_y))
 	screen.blit(display_lines, (1030 + scr_pos_x, 550 + scr_pos_y))
 
 	draw_black_box()
+
 	pygame.display.update()
 	clock +=1
 
