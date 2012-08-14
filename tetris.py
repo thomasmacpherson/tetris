a696e00a60f2305c243d129922601cedee7f00b9
thomasmacpherson <thomasmacpherson@live.co.uk>
2012-08-14 02:39:15 +0100

    finishing touches

--- tetris.py
+++ tetris.py
@@ -5,6 +5,7 @@ from math import log10
 
 scr_pos_x = -200
 scr_pos_y = 0
+in_play = True
 
 import pygame, time, sys, threading, random
 #import piface.pfio as pfio
@@ -12,6 +13,8 @@ import pygame, time, sys, threading, random
 from pygame.locals import *
 pygame.init()
 font = pygame.font.Font(None, 60)
+big_font = pygame.font.Font(None, 400)
+
 
 
 class Block(object):
@@ -51,8 +54,6 @@ class Shape(object):
 				Block(x+shape_index[shape][rot][0][2],y+shape_index[shape][rot][1][2],shape),
 				Block(x+shape_index[shape][rot][0][3],y+shape_index[shape][rot][1][3],shape)]
 
-
-
 	def move(self, x, y):
 		self.x += x
 		self.y += y
@@ -84,19 +85,22 @@ class Shape(object):
 			block.x = active_block.blocks[i].x
 			block.y = active_block.blocks[i].y
 
-
-
 	def draw(self):
 		for block in self.blocks:
 			block.draw()	
 
 	def delete(self):
 		global y_count
+
 		for block in self.blocks:
-			y_count[(block.y-626)/32] +=1
-			#print (block.y-626)/32
-			#print y_count
-			dead_block_list.append(block)
+			line_number = (block.y-626)/32
+			
+			if line_number < -18:
+				game_over()
+
+			else:
+				y_count[line_number] +=1
+				dead_block_list.append(block)
 		check_lines()
 		del(self)
 	
@@ -124,16 +128,11 @@ class Shape(object):
 		self.blocks[3].y = active_block.y + shape_index[active_block.shape][active_block.rot][1][3]
 
 		if check_collision(self):
-			#print "Rotation collision"
+
 			self.rot = previous_rot
 			for i, block in enumerate(self.blocks):
 				self.blocks[i].x = temp_blocks[i][0]
 				self.blocks[i].y = temp_blocks[i][1]
-
-
-
-
-
 
 def new_shape():
 	global active_block
@@ -158,14 +157,10 @@ def new_shape():
 	else:
 		next_shape = Shape(scr_pos_x + 933, scr_pos_y + 253, next_shape_number)
 
-
-
 	for x in range(1,20):
 		if shadow_block.y < (scr_pos_y + 594):
 			if shadow_block.move(0,32):
 				break
-
-
 
 def check_collision(shape):
 	for block in shape.blocks:
@@ -174,8 +169,6 @@ def check_collision(shape):
 		for dead_block in dead_block_list:
 			if block.x == dead_block.x and block.y == dead_block.y:
 				return True
-
-
 
 def check_lines():
 	global dead_block_list
@@ -187,19 +180,16 @@ def check_lines():
 			check_list.append(block.y)
 	check_list.sort()
 	check_list.reverse()
-	#print check_list
 	removed_line_count = 0
 	removed_list = list()
 	for y in check_list:
-		if check_line((y+(32*removed_line_count) - 82)/32):
+		if check_line((y+(32*removed_line_count) - 18)/32):
 			removed_list
 			removed_line_count += 1
-			# TODO: add flashing removal lines
-	#print "Removed line count: %d" %removed_line_count
 
 
 def check_line(line_number):
-	#print line_number
+
 	global dead_block_list
 
 	global score
@@ -207,49 +197,40 @@ def check_line(line_number):
 	global lines
 	global y_count
 
-	#print "Line number: %d" %line_number
+
 	if y_count[line_number] > 9:
-		#y_count[line_number] = 0
+
 		lines += 1
 		score += level*10
 		
 		if lines %10 == 0:
 			level += 1
 		for block in dead_block_list:
-			if block.y == (line_number*32)+82:
+			if block.y == (line_number*32)+18:
 				block.colour +=8 
 		draw_screen(False)
 		time.sleep(0.2)
 
-		dead_block_list = [block for block in dead_block_list if block.y != (line_number * 32)+82]
+		dead_block_list = [block for block in dead_block_list if block.y != (line_number * 32)+18]
 
 		for i in reversed(range(1,line_number+1)):
 			y_count[i] = y_count[i-1]
 
 			
 		for block in dead_block_list:		
-			if block.y < (line_number*32)+82:
+			if block.y < (line_number*32)+18:
 				block.y += 32
 		y_count[0] = 0
 		
 		return True
-
 	else:
 		return False
 
 
 
-
-
-
-"""
-		for block in dead_block_list:
-			if block.y == (line_number * 32)+82:
-				dead_block_list.remove(block)
-"""				
-
-
 def draw_screen(shadow):
+	global in_play
+	#global next_shape_help
 	screen.blit(background, (scr_pos_x + 335,scr_pos_y + 49))
 
 	screen.blit(background_images[level % len(background_images) ], (scr_pos_x + 510, scr_pos_y +49))
@@ -257,7 +238,9 @@ def draw_screen(shadow):
 	if shadow:
 		shadow_block.draw()
 	active_block.draw()
-	next_shape.draw()
+
+	if next_shape_help:
+		next_shape.draw()
 
 	
 	for block in dead_block_list:
@@ -272,10 +255,15 @@ def draw_screen(shadow):
 	screen.blit(display_level, (880 + scr_pos_x, 454 + scr_pos_y))
 	screen.blit(display_lines, (880 + scr_pos_x, 549 + scr_pos_y))
 
+	if not in_play:
+		display_game = big_font.render((" GAME"), 1, (150,0,0))
+		display_over = big_font.render((" OVER!"), 1, (150,0,0))
+		screen.blit(display_game, (200 + scr_pos_x, 60 + scr_pos_y))
+		screen.blit(display_over, (200 + scr_pos_x, 360 + scr_pos_y))
+
 	draw_black_box()
 
 	pygame.display.update()
-
 
 def draw_flashing_lines(line_list):
 	pass
@@ -285,10 +273,17 @@ def draw_black_box():
 	pygame.draw.rect(screen,(0,0,0), (scr_pos_x +450, scr_pos_y-50,1000,100),0)
 
 
+def game_over():
+	global score
+	global in_play
+	print "Your score was %d" %score
+	in_play = False
+	draw_screen(True)
+	exit()
+
 dead_block_list = list() # list of deadblocks for displaying and detecting collisions
 
-y_count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # list of number of blocks in each y position for full line detection
-
+y_count = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0] # list of number of blocks in each y position for full line detection
 
 pygame.init()
 
@@ -303,7 +298,7 @@ red_block = pygame.image.load("pngs/tetblock4.png").convert()
 green_block = pygame.image.load("pngs/tetblock5.png").convert()
 purple_block = pygame.image.load("pngs/tetblock6.png").convert()
 turq_block = pygame.image.load("pngs/tetblock7.png").convert()
-clear_block = pygame.image.load("pngs/tetblockclear.png").convert()
+clear_block = pygame.image.load("pngs/tetblockclear.png")	# not converted to keep png transparency
 dead_brown_block = pygame.image.load("pngs/tetblock1dead.png").convert()
 dead_beige_block = pygame.image.load("pngs/tetblock2dead.png").convert()
 dead_blue_block = pygame.image.load("pngs/tetblock3dead.png").convert()
@@ -335,13 +330,15 @@ block_colours = [brown_block,
 background_images = [ 	image1,
 			image2,
 			image3 ]
-
 score = 0
 lines = 0
 level = 1
 
 clock = 0
 speed = 15.0
+
+shadow = True
+next_shape_help = True
 
 active_block = Shape(scr_pos_x + 606,scr_pos_y + 82, random.randint(0,6))
 
@@ -352,8 +349,6 @@ for x in range(1,20):
 	if shadow_block.y < (scr_pos_y + 594):
 		if shadow_block.move(0,32):
 			break
-
-
 
 next_shape_number = random.randint(0,6)
 
@@ -387,7 +382,6 @@ while running == True:
 					if shadow_block.y < (scr_pos_y + 594):
 						if shadow_block.move(0,32):
 							break
-
 			elif event.key == K_LEFT:# and active_block.x >510:
 				active_block.move(-32,0)
 				shadow_block.position()
@@ -395,7 +389,6 @@ while running == True:
 					if shadow_block.y < (scr_pos_y + 594):
 						if shadow_block.move(0,32):
 							break
-
 			elif event.key == K_UP:
 				active_block.rotate()
 				shadow_block.position()
@@ -407,15 +400,17 @@ while running == True:
 				if active_block.y < 594:
 					if active_block.move(0,32):
 						new_shape()
-
 			elif event.key == K_SPACE:
 				for x in range(1,18):
 					if active_block.y < (scr_pos_y + 594):
 						if active_block.move(0,32):
 							break
 				new_shape()
+			elif event.key == K_s:
+				shadow = not shadow
 
-
+			elif event.key == K_h:
+				next_shape_help = not next_shape_help
 
 	current_speed = speed/level
 	if current_speed < 3:
@@ -426,11 +421,7 @@ while running == True:
 				new_shape()
 		else:
 			new_shape()
-
-	draw_screen(True)
-
+	draw_screen(shadow)
 	clock +=1
-
-
 exit()
 
